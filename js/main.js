b742a41991d7ed25fce37fc2b06b6a102d7dd631
Sam Beckbessinger <bcksam002@gmail.com>
2018-12-17 09:39:45 +0200

    More styling to js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 // Collapsable quiz things
 
-var coll = document.getElementsByClassName("card year");
+var coll = document.getElementsByClassName("card-year");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
     } else {
-      content.style.display = "block";
-    }
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
   });
 }
-
 // The countdown timer
