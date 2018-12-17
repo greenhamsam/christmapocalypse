b1945b5b3b667add7a891d98a02dc450a49d9857
Sam Beckbessinger <bcksam002@gmail.com>
2018-12-17 09:35:36 +0200

    Test collapsable

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Collapsable quiz things
+
+var coll = document.getElementsByClassName("card year");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+// The countdown timer
